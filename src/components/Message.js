39f4e8ae9494f5fbcf1016d379f1c58a5c07fb60
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-12 09:48:24 +1000

    Add button context consumer in Message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HelloContext } from '../contexts/hello-context';
+import ButtonContext from '../contexts/button-context';
 
 const Message = () => {
   return (
@@ -7,8 +8,18 @@ const Message = () => {
       {
         ({message, toggleMessage}) => (
           <header className="App-header">
-            <h1 className="App-title">{ message.firstWord + ' ' + message.secondWord }</h1>
-            <button onClick={ toggleMessage }>Change Message</button>
+            <h1 className="App-title">
+              { message.firstWord + ' ' + message.secondWord }
+            </h1>
+            <ButtonContext.Consumer>
+              {
+                ({title}) => (
+                  <button onClick={ toggleMessage }>
+                    { title }
+                  </button>
+                )
+              }
+            </ButtonContext.Consumer>
           </header>
         )
       }
