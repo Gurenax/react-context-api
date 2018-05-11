9682485b5324e9941589eab31d791f972676787b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-11 16:39:45 +1000

    Update App to use Context API

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
-import logo from '../assets/images/logo.svg'
+// import logo from '../assets/images/logo.svg'
 import '../styles/App.css'
 
+import { HelloContext, messages } from '../contexts/hello-context'
+import Message from './Message'
+
 class App extends Component {
+  // This function strictly needs to be declared before initialising the state
+  _toggleMessage = () => {
+    console.log('Test')
+    this.setState(prevState => ({
+      message:
+        prevState.message === messages.message1
+          ? messages.message2
+          : messages.message1
+    }))
+  }
+
+  state = {
+    message: messages.message1,
+    toggleMessage: this._toggleMessage,
+  }
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HelloContext.Provider value={ this.state }>
+        <div className="App">
+          <Message />
+        </div>
+      </HelloContext.Provider>
     )
   }
 }
