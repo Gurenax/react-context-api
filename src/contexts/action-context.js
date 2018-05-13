b18f1b2ef91bb1d19d6522ec605741ba0a1a2cc8
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-14 09:28:52 +1000

    Update action context to include async fire alert message

--- src/contexts/action-context.js
+++ src/contexts/action-context.js
@@ -5,6 +5,28 @@ export const fireAlertMessage = (message) => () => {
   alert(message)
 }
 
+// Return async function to be invoked later when button is clicked
+export const fireAsyncAlertMessage = (message, duration) => async () => {
+  const theMessage = await messagePromise(message, duration)
+  alert(theMessage)
+}
+
+// Returns a promise containing a message
+const messagePromise = (message, duration) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(!!message) {
+        resolve(message)
+      }
+      else {
+        reject('Empty message found')
+      }
+    }, duration)
+  })
+}
+
+// Action Context
 export const ActionContext = React.createContext({
   alertMessage: fireAlertMessage,
+  asyncAlertMessage: fireAsyncAlertMessage
 })
